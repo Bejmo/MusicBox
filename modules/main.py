c83f2a2b9f0dff8759e481a67c88230797da896e
Bejmo <bejmo24@gmail.com>
2025-07-23 17:03:29 +0200

    feat: se pueden guardar las playlists
    Se guardan las playlists en memoria para poder actualizarlas más cómodamente

--- modules/main.py
+++ modules/main.py
@@ -1,27 +1,37 @@
 from download_music import descargar_musica
 from yt_dlp import YoutubeDL
-import os
 from paths import PATHS
+import os
+import csv
 
+# Global variables to store the name of the file that contains the url's of the songs to download
 url_file = ""
 url_file_name = "terminal_input"
+
+
+# Download playlist functions
 
 """
 Downloads the new songs of the playlist (the ones that are now downloaded on the devide)
 """
-def update_playlist(url):
+def get_new_songs(url):
     # Options yt-dlp
     ydl_opts = {
         'quiet': True,
         'extract_flat': True,
         'force_generic_extractor': True,
         'restrictfilenames': True,
-        'outtmpl': '%(title)s__%(id)s', #  TODO Antes: os.path.join(PATHS['download_folder'], '%(title)s__%(id)s.%(ext)s')
+        'outtmpl': '%(title)s__%(id)s'
     }
 
     # Execute yt-dlp
     with YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
+        try:
+            info = ydl.extract_info(url, download=False)
+        except Exception as e:
+            print(f"Error extracting info: {e}")
+            raise Exception("Failed to extract information from the URL.")
+            return "not_a_playlist"
 
         if 'entries' in info: # It is a playlist
             """
@@ -73,7 +83,10 @@ def update_playlist(url):
 
         return playlist_name
 
-def terminal():
+"""
+Downloads the files.
+"""
+def download():
     with open(url_file, 'r') as file:
         list_urls = file.readlines()
     os.remove(url_file)
@@ -86,17 +99,148 @@ def terminal():
         except:
             print(f"ERROR - No se ha podido descargar la canción con URL: {url}. Más información en el fichero \"error_logs_unavailable.txt\"")
 
-def main():
-    # Input user
-    url = input("Introduzca la URL (de la playlist o la canción): ")
 
-    # Computations
-    playlist_name = update_playlist(url)
+# INPUT FUNCTIONS
+
+"""
+Asks the user if they want to save the used playlist
+"""
+def ask_if_save_playlist(url, name_playlist):
+    playlist_file_name = "playlists.csv"
+
+    response = ""
+    allowed_values = ["S", "s", "N", "n"]
+    while not response in allowed_values:
+        if response != "": print("Valor inválido. Vuelva a intentarlo")
+        response = input("Quiere guardar la playlist usada? (s/n) ")
+
+    if response == "S" or response == "s":
+        with open(playlist_file_name, "a", newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=["nombre_playlist","url"])
+            # We have to write the header if the file does not exists or has nothing inside.
+            if not os.path.exists(playlist_file_name) or os.path.getsize(playlist_file_name) == 0:
+                writer.writeheader()
+            writer.writerow({"nombre_playlist": name_playlist, "url": url})
+
+"""
+Charges the playlists saved in memory previously
+"""
+def charge_playlists():
+    playlist_file_name = "playlists.csv"
+
+    playlists = []
+    if os.path.exists(playlist_file_name): # If exists, it uses it
+        with open(playlist_file_name, "r", newline='') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                playlists.append([row["nombre_playlist"], row["url"]])
+    else: # Create the file if it doesn't exist
+        with open(playlist_file_name, "w", newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=["nombre_playlist","url"])
+            writer.writeheader()
+
+    return playlists
+
+"""
+Deletes the playlist in the "i" row 
+"""
+def delete_playlist(i):
+    playlist_file_name = "playlists.csv"
+
+    # Read all the rows except from the ones that we want to delete
+    new_rows = []
+    with open(playlist_file_name, "r", newline='') as f:
+        reader = csv.DictReader(f)
+        j = 1
+        for row in reader:
+            if i != j: new_rows.append(row)
+            j += 1
+
+    # Overwrite the file
+    with open(playlist_file_name, "w", newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=["nombre_playlist", "url"])
+        writer.writeheader()
+        writer.writerows(new_rows)
+
+"""
+Downloads the playlist given its url.
+Returns True if it is a playlist, False otherwise.
+"""
+def download_playlist(url):
+    playlist_name = get_new_songs(url)
     if playlist_name == "not_a_playlist":
         print("Se ha finalizado la descarga de la canción.")
+        return False
+
+    download()
+    print(f"Se ha finalizado la actualización de la playlist {playlist_name}")
+    return playlist_name
+
+"""
+Main function
+"""
+def main():
+    # Read saved playlists
+    playlists = charge_playlists()
+
+    # No playlists found, then we use it normally
+    if not playlists:
+        url = input("Introduzca la URL (de la playlist o la canción): ")
+        name_playlist = download_playlist(url)
+        if name_playlist: ask_if_save_playlist(url, name_playlist)
+
+    # Exists saved playlists
     else:
-        terminal()
-        print(f"Se ha finalizado la actualización de la playlist {playlist_name}")
+        num_playlists = len(playlists)
+
+        # Select if the user wants to use a saved playlist
+        i = -2
+        first = True # This is for making a print when the user puts an invalid input
+        while i < -1 or i > num_playlists:
+            if not first: print("Número inválido. Vuelva a intentarlo.")
+            else:
+                first = False
+                print("Opciones extra:")
+                print("-1: Eliminar playlist existente")
+                print("0: Introducir playlist manualmente")
+
+                print("Playlists guardadas: ")
+                i = 0
+                for playlist in playlists:
+                    print(f"{i + 1}: {playlist[0]}")
+                    i += 1
+                    
+            try:
+                i = int(input("Seleccione el índice de la playlist a actualizar: "))
+            except ValueError: i = -2
+
+        # The user wants to delete an existing playlist
+        if i == -1:
+            print("Playlists guardadas: ")
+            j = 0
+            for playlist in playlists:
+                print(f"{j + 1}: {playlist[0]}")
+                j += 1
+
+            first = True
+            while i < 1 or i > num_playlists:
+                if not first: print("Índice inválido")
+                try:
+                    i = int(input("¿Qué playlist desea eliminar?: "))
+                except ValueError: i = -2
+
+            delete_playlist(i)
+
+        # The user wants to use a new playlist
+        elif i == 0:
+            url = input("Introduzca la URL (de la playlist o la canción): ")
+            name_playlist = download_playlist(url)
+            if name_playlist: ask_if_save_playlist(url, name_playlist)
+         
+        # The user wants to use a saved playlist
+        else:
+            url = playlists[i - 1][1]
+            download_playlist(url)
 
 if __name__ == "__main__":
     main()
