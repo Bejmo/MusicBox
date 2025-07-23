641649f24fbe9aafc6ddfc75d5318f56da4a3f42
Bejmo <bejmo24@gmail.com>
2025-07-23 20:06:46 +0200

    Publish

--- kivy/terminal.py
+++
@@ -1,9 +0,0 @@
-from modules.download_music import descargar_video
-
-def terminal():
-    with open('terminal_input.txt', 'r') as file:
-        list_urls = file.readlines()
-    for url in list_urls:
-        descargar_video(url.strip())
-
-terminal()
